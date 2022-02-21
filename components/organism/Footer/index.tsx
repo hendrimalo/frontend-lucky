4341ed9bbb30bd65f64e98d4b1dcb858983e43d1
hendrimalo <hendri.malo@yahoo.co.id>
2022-02-21 20:38:42 +0700

    add footer contact

--- components/organism/Footer/index.tsx
+++ components/organism/Footer/index.tsx
@@ -3,10 +3,18 @@ const Footer = function () {
     <div
       id="about-us"
       className="footer text-center mt-4"
-      style={{ height: '160px' }}
+      style={{ height: '130px' }}
     >
       <h2 className="display-5 font-weight-bold mt-5">Lucky Barbershop</h2>
       <p>As Good As You Will Be</p>
+      <div className="col">
+        <a href="https://www.instagram.com/cukurlucky/" target="_blank" rel="noreferrer">
+          <img src="/images/logo-instagram.png" alt="" style={{ width: '30px', height: '30px', marginRight: '8px' }} />
+        </a>
+        <a href="https://wa.link/dvwa5w" target="_blank" rel="noreferrer">
+          <img src="/images/logo-whatsapp.png" alt="" style={{ width: '30px', height: '30px' }} />
+        </a>
+      </div>
     </div>
   );
 };

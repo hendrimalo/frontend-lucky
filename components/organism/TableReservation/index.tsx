96c5784b4c4e1f8454f18813a882f392aea003ea
hendrimalo <hendri.malo@yahoo.co.id>
2022-03-10 00:32:03 +0700

    fix index table reservation

--- components/organism/TableReservation/index.tsx
+++ components/organism/TableReservation/index.tsx
@@ -16,28 +16,26 @@ const TableReservation = function () {
   }, []);
 
   return (
-    <div className="card">
-      <table className="table table-striped table-dark">
-        <thead>
-          <tr>
-            <th scope="col">No</th>
-            <th scope="col">Username</th>
-            <th scope="col">Date</th>
-            <th scope="col">Time</th>
+    <table className="table table-striped table-dark">
+      <thead>
+        <tr>
+          <th scope="col">No</th>
+          <th scope="col">Username</th>
+          <th scope="col">Date</th>
+          <th scope="col">Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {dataReservation.map((reservation: ReservationTypes, index) => (
+          <tr key={reservation._id}>
+            <th scope="row">{index + 1}</th>
+            <td>{reservation.username}</td>
+            <td>{reservation.date}</td>
+            <td>{reservation.time}</td>
           </tr>
-        </thead>
-        <tbody>
-          {dataReservation.map((reservation: ReservationTypes) => (
-            <tr key={reservation._id}>
-              <th scope="row">1</th>
-              <td>{reservation.username}</td>
-              <td>{reservation.date}</td>
-              <td>{reservation.time}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
